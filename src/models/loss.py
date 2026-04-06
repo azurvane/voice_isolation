c6f4970a509f76abff07e26bc01e9dc3c1096a56
azurvane <179304994+azurvane@users.noreply.github.com>
2026-04-06 14:09:45 +0530

    add loss custom loss
    add Scale-Invariant Signal-to-Distortion Ratio which mesuare how well a predicted waveform matches a target, regardless of volume scaling in function _si_sdr and added Permutation Invariant Training (pit_loss) which compute loss for every permutation of the speakers because architecture does not produce the output audio in any fixed sequence so speaker 1 and speaker 2 in out can be output1 and output2 or output2 and output1 so we need to calculate loss for output1 against speaker1 and speaker2 and for output2 against speaker1 and speaker2 and take the minimum loss for each output aginst all speakers

---
+++ src/models/loss.py
@@ -0,0 +1,40 @@
+import torch
+from itertools import permutations
+
+
+def _si_sdr(predicted, target):
+    epsilon = 1e-8
+    
+    predicted = predicted - torch.mean(predicted, dim=-1, keepdim=True)
+    target = target - torch.mean(target, dim=-1, keepdim=True)
+    
+    dot = torch.sum(predicted * target, dim=-1)
+    power = torch.sum(target * target, dim=-1) + epsilon
+    
+    s_target = (dot/power).unsqueeze(-1) * target
+    e_noise = predicted - s_target
+    
+    return 10 * torch.log10(torch.sum(torch.pow(s_target, 2), dim=-1)/(torch.sum(torch.pow(e_noise, 2), dim=-1) + epsilon))
+
+def pit_loss(outputs, targets, train=True):
+    num_speaker = outputs.shape[1]
+    perm_iterator = permutations(range(num_speaker))
+    perm_list = list(perm_iterator)
+    
+    loss_list = []
+    
+    for perm in perm_list:
+        speaker_losses = []
+        for count, j in enumerate(perm):
+            speaker_loss = _si_sdr(outputs[:, j, :], targets[:, count, :])
+            speaker_losses.append(speaker_loss)
+        perm_loss = -torch.stack(speaker_losses, dim=0).mean(dim=0)
+        loss_list.append(perm_loss)
+    
+    loss_stack = torch.stack(loss_list, dim=0)
+    min_loss = torch.min(loss_stack, dim=0).values
+    
+    if train == True:
+        return min_loss.mean()
+    else:
+        return min_loss
